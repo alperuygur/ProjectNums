beb453ab77eaec2c6ebbb63102f302055fa783ca
Alper Uygur <alper.uyg@gmail.com>
2022-09-01 21:39:26 +0300

    screenshots added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 description = """
 ## Examples:
 * ● input: 500 output 500
-* ● input: 3400 outputs 3.5k
+* ● input: 3400 outputs 3.4K
 * ● input: 1000000 output: 1M
 * ● input: 2500000.34 output: 2.5M
 * ● input: 1123456789 output: 1.1B
